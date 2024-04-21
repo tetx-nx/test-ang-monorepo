51e2e434a5893a573556a621c3b7fa7d7932e5a8
Vasiliy Mazhekin <mazhekin@gmail.com>
2024-04-21 12:40:52 +0200

    add orders project

--- libs/orders/src/lib/orders/orders.component.ts
+++ libs/orders/src/lib/orders/orders.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-products',
+  selector: 'lib-orders',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.scss',
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.scss',
 })
-export class ProductsComponent {}
+export class OrdersComponent {}
